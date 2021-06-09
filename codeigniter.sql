e0f6d507af2b7fa6223b2b6a15df101680185e0e
omkarchoudhary <omkarchoudhary2012@gmail.com>
2021-06-09 16:18:44 +0530

    Update database file

--- codeigniter.sql
+++ codeigniter.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 06, 2021 at 08:04 PM
+-- Generation Time: Jun 09, 2021 at 10:31 AM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 7.3.28
 
@@ -31,24 +31,106 @@ CREATE TABLE `article` (
   `id` int(11) NOT NULL,
   `article_title` varchar(150) NOT NULL,
   `article_body` longtext NOT NULL,
-  `user_id` int(11) NOT NULL
+  `user_id` int(11) NOT NULL,
+  `image_path` varchar(300) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `article`
 --
 
-INSERT INTO `article` (`id`, `article_title`, `article_body`, `user_id`) VALUES
-(1, 'er', 'qw', 1),
-(3, 'gsdfg', 'gs', 1),
-(4, 'fdasgdsg', 'fdsafasdf', 1),
-(5, 'jh', 'hkgk', 1),
-(6, 'hgf', 'jgfj', 1),
-(9, 'Mohanarticle', 'newone', 3),
-(10, 'fdh', 'hfd', 3),
-(11, 'abhinav', 'sharma', 1),
-(12, 'dsg', 'gdsgdsgdg', 1),
-(13, 'gfj', 'gjfj', 1);
+INSERT INTO `article` (`id`, `article_title`, `article_body`, `user_id`, `image_path`) VALUES
+(1, 'er', 'qw', 1, ''),
+(3, 'gsdfg', 'gs', 1, ''),
+(4, 'fdasgdsg', 'fdsafasdf', 1, ''),
+(5, 'jh', 'hkgk', 1, ''),
+(6, 'hgf', 'jgfj', 1, ''),
+(9, 'Mohanarticle', 'newone', 3, ''),
+(10, 'fdh', 'hfd', 3, ''),
+(11, 'abhinav', 'sharma', 1, ''),
+(12, 'dsg', 'gdsgdsgdg', 1, ''),
+(13, 'gfj', 'gjfj', 1, ''),
+(17, 'mmmmmmmmmmmmm', 'bbbbbbbbbbb', 1, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `city`
+--
+
+CREATE TABLE `city` (
+  `city_id` int(10) NOT NULL,
+  `state_id` int(10) DEFAULT NULL,
+  `city_name` varchar(150) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `city`
+--
+
+INSERT INTO `city` (`city_id`, `state_id`, `city_name`) VALUES
+(1, 1, 'New York city'),
+(2, 1, 'Buffalo'),
+(3, 1, 'Albany'),
+(4, 2, 'Birmingham'),
+(5, 2, 'Montgomery'),
+(6, 2, 'Huntsville'),
+(7, 3, 'Los Angeles'),
+(8, 3, 'San Francisco'),
+(9, 3, 'San Diego'),
+(10, 4, 'Toronto'),
+(11, 4, 'Ottawa'),
+(12, 5, 'Vancouver'),
+(13, 5, 'Victoria'),
+(14, 6, 'Sydney'),
+(15, 6, 'Newcastle'),
+(16, 7, 'City of Brisbane'),
+(17, 7, 'Gold Coast'),
+(18, 8, 'Bangalore'),
+(19, 8, 'Mangalore'),
+(20, 9, 'Hydrabad'),
+(21, 9, 'Warangal');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `country`
+--
+
+CREATE TABLE `country` (
+  `country_id` int(10) NOT NULL,
+  `country_name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `country`
+--
+
+INSERT INTO `country` (`country_id`, `country_name`) VALUES
+(1, 'USA'),
+(2, 'Canada'),
+(3, 'Australia'),
+(4, 'India');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `id` int(5) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(150) DEFAULT NULL,
+  `feedback1` varchar(150) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`id`, `name`, `email`, `feedback1`) VALUES
+(1, 'fd', 'fdsfg', 'fdsg');
 
 -- --------------------------------------------------------
 
@@ -72,6 +154,33 @@ CREATE TABLE `register` (
 INSERT INTO `register` (`id`, `first_name`, `last_name`, `address`, `email`, `mobile`) VALUES
 (16, 'sohansd', 'fhbdkj', 'kjk', 'jkbk', '6894325'),
 (17, 'sohan', 'fhbdkj', 'kjk', 'jkbk', '6894325');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `state`
+--
+
+CREATE TABLE `state` (
+  `state_id` int(10) NOT NULL,
+  `country_id` int(10) DEFAULT NULL,
+  `state_name` varchar(150) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `state`
+--
+
+INSERT INTO `state` (`state_id`, `country_id`, `state_name`) VALUES
+(1, 1, 'New York'),
+(2, 1, 'Alabama'),
+(3, 1, 'California'),
+(4, 2, 'Ontario'),
+(5, 2, 'British Columbia'),
+(6, 3, 'New South Wales'),
+(7, 3, 'Queensland'),
+(8, 4, 'Karnataka'),
+(9, 4, 'Telangana');
 
 -- --------------------------------------------------------
 
@@ -110,10 +219,36 @@ ALTER TABLE `article`
   ADD KEY `user_id` (`user_id`);
 
 --
+-- Indexes for table `city`
+--
+ALTER TABLE `city`
+  ADD PRIMARY KEY (`city_id`),
+  ADD KEY `state_id` (`state_id`);
+
+--
+-- Indexes for table `country`
+--
+ALTER TABLE `country`
+  ADD PRIMARY KEY (`country_id`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `register`
 --
 ALTER TABLE `register`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `state`
+--
+ALTER TABLE `state`
+  ADD PRIMARY KEY (`state_id`),
+  ADD KEY `country_id` (`country_id`);
 
 --
 -- Indexes for table `users`
@@ -129,13 +264,37 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `article`
 --
 ALTER TABLE `article`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `city`
+--
+ALTER TABLE `city`
+  MODIFY `city_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `country`
+--
+ALTER TABLE `country`
+  MODIFY `country_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `register`
 --
 ALTER TABLE `register`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `state`
+--
+ALTER TABLE `state`
+  MODIFY `state_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -152,6 +311,18 @@ ALTER TABLE `users`
 --
 ALTER TABLE `article`
   ADD CONSTRAINT `article_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `city`
+--
+ALTER TABLE `city`
+  ADD CONSTRAINT `city_ibfk_1` FOREIGN KEY (`state_id`) REFERENCES `state` (`state_id`);
+
+--
+-- Constraints for table `state`
+--
+ALTER TABLE `state`
+  ADD CONSTRAINT `state_ibfk_1` FOREIGN KEY (`country_id`) REFERENCES `country` (`country_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
